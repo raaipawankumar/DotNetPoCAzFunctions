b88bb7d00f3205083f8928a588442e75c40552b8
Pawan kumar Rai <pawankumarraai@gmail.com>
2025-03-03 21:44:57 +0530

    Remove user secrets configuration

--- src/DotNetPoC.Functions/Program.cs
+++ src/DotNetPoC.Functions/Program.cs
@@ -5,19 +5,16 @@ using Azure.Storage.Blobs;
 using static System.Environment;
 using DotNetPoC.Functions.Shared;
 using SendGrid;
-using Microsoft.Extensions.Configuration;
-
 
 var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
-builder.Configuration.AddUserSecrets("c5d8bba1-c2ea-4e52-9a16-2ea72da91501");
 
-builder.Services.AddSingleton<BlobContainerClient>(pr =>
+builder.Services.AddSingleton(() =>
   new BlobServiceClient(GetEnvironmentVariable("Storage"))
     .GetBlobContainerClient(GetEnvironmentVariable("PDFContainerName")));
 builder.Services.AddSingleton<CloudStorage>();
 
-builder.Services.AddSingleton<SendGridClient>(pr =>
+builder.Services.AddSingleton(() =>
 {
   var key = GetEnvironmentVariable("SENDGRID_KEY");
   return new SendGridClient(key);
